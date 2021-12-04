a819d0f1a1e49fc76e20897e276b3f821d2efe76
Yohaan <80580077+Yohaan123@users.noreply.github.com>
2021-12-04 16:05:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 status = "";
+object = [];
+synth = window.speechSynthesis;
+utterThis = new SpeechSynthesisUtterance(inputbox.value);
 
 function setup(){
     canvas = createCanvas(480,380);
@@ -19,6 +22,38 @@ function modelLoaded(){
     status = true;
 }
 
+function gotResult(error,results){
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        object = results;
+    }
+}
+
 function draw(){
     image(video,0,0,480,380);
+    if(status != ""){
+        for(i = 0; i < object.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            fill("red");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+    if(object = inputbox){
+       variable_name_holds_WebcamLiveView.stop();
+       objectDetector.detect(gotResult);
+       document.getElementById("status").innerHTML = inputbox.value + " found";
+       synth = window.speechSynthesis;
+       utterThis = new SpeechSynthesisUtterance(inputbox.value);
+       synth.speak("utterThis");
+    }
+    else{
+        document.getElementById("status").innerHTML = inputbox.value + " not found";
+    }
 }
